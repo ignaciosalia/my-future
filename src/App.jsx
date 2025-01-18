323e917b0985e8b27618cc5d393a7ba69f01c97b
ignaciosalia <ignaciosalia@gmail.com>
2025-01-17 22:20:22 -0600

    se crea maqueta le parte principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,27 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
- 
+
   const [cargo, setCargo] = useState(false)
+  const [menuSelect, setMenuSelect] = useState(false)
+
+  const menuSeleccionado = (seccion) => {
+    console.log(seccion)
+    switch(seccion){
+      case "front":
+        setMenuSelect('front')
+        break
+      case "movil":
+        setMenuSelect('movil')
+        break
+      case "nube":
+        setMenuSelect('nube')
+        break
+      case "geo":
+        setMenuSelect('geo')
+        break
+    }
+  }
 
   function handleContainerResize() {
     const width = document.getElementById("contenedor").offsetWidth;
@@ -12,32 +31,39 @@ function App() {
   }
 
   useEffect(() => {
-    if(!cargo){
+    if (!cargo) {
       console.log(cargo)
       handleContainerResize()
       window.addEventListener("resize", handleContainerResize);
-      
+
       Split(["#izquierda", "#derecha"], {
         sizes: [49, 50],
       });
       setCargo(true)
     }
-    
-  },[cargo])
+
+  }, [cargo])
 
   return (
     <>
       <h1 className='maquina-de-escribir'>Ignacio Salia</h1>
-      <p>Desarrollador frontend</p> 
+      <p>Desarrollador frontend</p>
       <div id="contenedor">
         <div id="izquierda" >
-          <div id="map-left" style={{width:"50%"}}>
+          <div id="map-left" style={{ width: "50%" }}>
             <h2>Creando experiencias</h2>
+            <menu>
+              <li className={menuSelect === "front" ? 'elemento-select' : ""} onClick={() => menuSeleccionado('front')}>Frontend</li>
+              <li className={menuSelect === "movil" ? 'elemento-select' : ""} onClick={() => menuSeleccionado('movil')}>Móvil</li>
+              <li className={menuSelect === "nube" ? 'elemento-select' : ""} onClick={() => menuSeleccionado('nube')}>Nube</li>
+              <li className={menuSelect === "geo" ? 'elemento-select' : ""} onClick={() => menuSeleccionado('geo')}>Geografía</li>
+            </menu>
           </div>
         </div>
         <div id="derecha" >
-          <div id="map-right" style={{width:"50%"}}>
-            <h2 style={{textAlign:"right"}}>Ayudando con inovación</h2>
+          <div id="map-right" style={{ width: "50%" }}>
+            <h2 style={{ textAlign: "right" }}>Ayudando con inovación</h2>
+
           </div>
         </div>
       </div>
